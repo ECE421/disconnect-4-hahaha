89eaac7bfca5d988b6e669178c8cb0d674551a37
Thomas Lorincz <tlorincz@ualberta.ca>
2019-03-22 10:42:59 -0600

    Changed css_provider to be an instance variable to improve draw performance.

--- src/game_board/game_board_view.rb
+++ src/game_board/game_board_view.rb
@@ -6,19 +6,17 @@ class GameBoardView
 
   def initialize(window)
     @window = window # Reference to the application window
+    @css_provider = Gtk::CssProvider.new
+    @css_provider.load(data: File.read('./src/game_board/game_board.css'))
   end
 
   def draw(board_data)
-    css_provider = Gtk::CssProvider.new
-    css_provider.load(data: File.read('./src/game_board/game_board.css'))
-
     grid = Gtk::Grid.new
-    @window.add(grid)
 
     (0..6).each do |col|
       button = Gtk::Button.new
       button.set_size_request(100, 600)
-      button.style_context.add_provider(css_provider, Gtk::StyleProvider::PRIORITY_USER)
+      button.style_context.add_provider(@css_provider, Gtk::StyleProvider::PRIORITY_USER)
       button.signal_connect('clicked') do |_|
         changed
         notify_observers('column_clicked', col)
@@ -26,6 +24,7 @@ class GameBoardView
       grid.attach(button, col, 0, 1, 1)
     end
 
+    @window.add(grid)
     @window.show_all
   end
 end
