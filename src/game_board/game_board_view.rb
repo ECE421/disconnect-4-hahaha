cbdd62231ff3606d9125dd79605f61259384dbee
Thomas Lorincz <tlorincz@ualberta.ca>
2019-03-22 10:31:37 -0600

    Buttons are now columns. Styling is moved to a CSS file.

--- src/game_board/game_board_view.rb
+++ src/game_board/game_board_view.rb
@@ -14,35 +14,22 @@ class GameBoardView
     @window.add(grid)
 
     css_provider = Gtk::CssProvider.new
-    css_provider.load(data: <<-CSS)
-      button {
-        background-image: image(blue);
-      }
-
-      button:hover {
-        background-image: image(purple);
-      }
-    CSS
+    css_provider.load(data: File.read('./src/game_board/game_board.css'))
 
     (0..6).each do |col|
-      (0..5).each do |row|
-        button = Gtk::Button.new
-        button.set_size_request(100, 100)
-        button.style_context.add_provider(
-          css_provider,
-          Gtk::StyleProvider::PRIORITY_USER
-        )
-        button.signal_connect 'clicked' do |_|
-          puts 'Hello World!!'
-        end
-        grid.attach(button, col, row, 1, 1)
+      button = Gtk::Button.new
+      button.set_size_request(100, 600)
+      button.style_context.add_provider(
+        css_provider,
+        Gtk::StyleProvider::PRIORITY_USER
+      )
+      button.signal_connect('clicked') do |_|
+        changed
+        notify_observers('column_clicked', col)
       end
+      grid.attach(button, col, 0, 1, 1)
     end
 
     @window.show_all
-  end
-
-  def on_click(element)
-    # notify_observers(element_data)
   end
 end
