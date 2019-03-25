a66539af81612a2d32b9b9af87e80f06154da78c
zdrever <zdrever@ualberta.ca>
2019-03-25 12:38:20 -0600

    OTTOTOOT win state tests

--- test/app_model_test.rb
+++ test/app_model_test.rb
@@ -62,5 +62,50 @@ class AppModelTest < Test::Unit::TestCase
     @model.board_place_token(3) #1
     assert_true(@model.connect_4_left_diagonal?)
   end
-  
+
+  def test_ot_horiz
+    @model.place_token(0)
+    @model.place_token(1)
+    @model.place_token(3)
+    @model.board_place_token(2)
+    assert_equal(1, @model.toot_and_otto_horizontal)
+  end
+
+  def test_ot_vert
+    @model.place_token(0)
+    @model.place_token(0)
+    @model.place_token(1)
+    @model.place_token(0)
+    @model.board_place_token(0)
+    assert_equal(1, @model.toot_and_otto_vertical)
+  end
+
+  def test_ot_right_diag
+    @model.place_token(0) #1
+    @model.place_token(2) #2
+    @model.place_token(1) #1
+    @model.place_token(1) #2
+    @model.place_token(2) #1
+    @model.place_token(2) #2
+    @model.place_token(3) #1
+    @model.place_token(3) #2
+    @model.place_token(3) #1
+    @model.place_token(4) #2
+    @model.board_place_token(3)
+    assert_equal(1, @model.toot_and_otto_right_diagonal)
+  end
+
+  def test_ot_left_diag
+    @model.place_token(0) #1
+    @model.place_token(0) #2
+    @model.place_token(0) #1
+    @model.place_token(1) #2
+    @model.place_token(0) #1
+    @model.place_token(1) #2
+    @model.place_token(2) #1
+    @model.place_token(1) #2
+    @model.place_token(3) #1
+    @model.board_place_token(2)
+    assert_equal(1, @model.toot_and_otto_left_diagonal)
+  end
 end
